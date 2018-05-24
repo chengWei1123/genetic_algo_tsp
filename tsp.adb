98d51ce9cae06d5fb9554b9649d7aa878b43f016
chengWei1123 <c09721123@gmail.com>
2018-05-25 01:41:51 +0800

    change crossover method

--- tsp.adb
+++ tsp.adb
@@ -12,6 +12,9 @@ procedure tsp is
     population_size : integer := 1000;
     --length of gene = number of node 
     gene_length : integer := 15;
+    max_dist:integer := 40;
+    min_dist:integer := 10;
+    mutation_rate:integer:=100;
     Task_number : integer;
     Task_Load : integer;
     type gene is array (integer range 1..gene_length) of integer range 1..gene_length;
@@ -21,25 +24,23 @@ procedure tsp is
     parent : container;
     childarray   :container;
 	--use map(i)(j) to access map[i][j]
-    map : array(integer range 1..gene_length) of map_row:=
-(( 0,  6, 17, 23, 27, 16, 20, 22, 13, 24, 27, 16, 10, 24, 29),
-(  6,  0,  3, 13, 12, 13, 28, 10, 19, 26, 22, 20, 28, 13, 21),
-( 17,  3,  0,  1, 22, 19, 27, 13, 15, 18, 14, 20, 24, 18, 17),
-( 23, 13,  1,  0,  6, 11, 26, 24, 24, 14, 15, 10, 18, 25, 25),
-( 27, 12, 22,  6,  0,  9, 17, 28, 18, 25, 14, 23, 15, 19, 13),
-( 16, 13, 19, 11,  9,  0,  5, 19, 10, 25, 25, 28, 10, 25, 13),
-( 20, 28, 27, 26, 17,  5,  0,  4, 12, 17, 23, 16, 26, 23, 18),
-( 22, 10, 13, 24, 28, 19,  4,  0,  5, 26, 28, 14, 10, 28, 21),
-( 13, 19, 15, 24, 18, 10, 12,  5,  0,  1, 14, 10, 19, 20, 20),
-( 24, 26, 18, 14, 25, 25, 17, 26,  1,  0,  7, 28, 24, 23, 20),
-( 27, 22, 14, 15, 14, 25, 23, 28, 14,  7,  0,  8, 23, 10, 19),
-( 16, 20, 20, 10, 23, 28, 16, 14, 10, 28,  8,  0,  6, 26, 18),
-( 10, 28, 24, 18, 15, 10, 26, 10, 19, 24, 23,  6,  0,  5, 12),
-( 24, 13, 18, 25, 19, 25, 23, 28, 20, 23, 10, 26,  5,  0,  5),
-( 29, 21, 17, 25, 13, 13, 18, 21, 20, 20, 19, 18, 12,  5,  0));
+    map : array(integer range 1..gene_length) of map_row;
 
     fitness : array(integer range 1..population_size) of integer;
-
+    --generate random map
+    procedure generate_map is
+    begin
+        for i in 1..gene_length loop
+            for j in i..gene_length loop
+                map(i)(j) := (random(g) mod max_dist-min_dist+1)+min_dist;
+                map(j)(i) := map(i)(j);
+                if i=j then
+                    map(i)(j) := 0;
+                end if;    
+            end loop;
+        end loop;
+    end generate_map;
+    
 	--generate first generation
     procedure generate_first is 
         rand_pos : integer; 
@@ -78,73 +79,34 @@ procedure tsp is
         return (population_size);
     end selection;
 
-    --check if exist same element in array yes:1 no:0
-    function checksame(TestArray : gene;TestPosition : integer) return integer is 
-        ans : integer := 0;
-        begin
-            for i in 1 .. gene_length loop
-                if i=TestPosition then 
-                    put("");
-                else
-                    if TestArray(i)=TestArray(TestPosition) then
-                        ans := 1;
-                    end if; 
-                end if;
-            end loop;
-        return ans;
-    end checksame;
-
-    --generate the child
-    procedure childgenerate  is
-        
-        P1 : integer;
-        P2 : integer;
-        start : integer;
-        last  : integer;
-        swaptmp   : integer;
-        exam :integer :=0;
+    procedure crossover(c_i:integer) is
+        p:integer;
+        longest:integer:=1;
+        tmp:integer;
+        r1:integer;
+        r2:integer;
     begin
-        P1  := selection;
-        P2  := selection;
-        start   :=  (Random(g) mod gene_length)+1;
-        if gene_length-start = 0 then
-            last    :=  start;
-        else
-            last    :=  (Random(g) mod (gene_length-start))+start;
-        end if;
-
-        for i in start .. last loop
-            swaptmp := childarray(P1)(i);
-            childarray(P1)(i) := childarray(P2)(i);
-            childarray(P2)(i) := swaptmp;
-        end loop;
-
-        for i in start .. last loop
-            exam :=0;
-            if checksame(childarray(P1),i)=1 then
-               for j in start .. last loop
-                    if checksame(childarray(P2),j)=1 and exam =0 then
-                                    swaptmp := childarray(P1)(i);
-                                    childarray(P1)(i) := childarray(P2)(j);
-                                    childarray(P2)(j) := swaptmp;
-                                    exam :=1;
-                    end if;
-                end loop;
+        p:=selection;
+        for i in 1..gene_length-1 loop
+            if map(parent(p)(i))(parent(p)(i+1)) > map(parent(p)(longest))(parent(p)(longest+1)) then
+                longest:=i;
             end if;
         end loop;
-        ----------------------------- Mutation P1
-        
-        start := Random(g) mod 100;
-        if start = 7 then
-            start := (Random(g) mod gene_length)+1;
-            last := (Random(g) mod gene_length)+1;
-            swaptmp := childarray(P1)(start);
-            childarray(P1)(start) := childarray(P1)(last);
-            childarray(P1)(last) := swaptmp;
+        for i in 1..gene_length loop
+            if i + longest <= gene_length then 
+                childarray(c_i)(i):=parent(p)(i+longest);
+            else
+                childarray(c_i)(i):=parent(p)((i+longest)mod gene_length);
+            end if;
+        end loop;
+        if random(g) mod mutation_rate = 1 then
+            r1:=random(g) mod gene_length +1;
+            r2:=random(g) mod gene_length +1;
+            tmp:=childarray(c_i)(r1);
+            childarray(c_i)(r1):=childarray(c_i)(r2);
+            childarray(c_i)(r2):=tmp;
         end if;
-       
-
-    end childgenerate;
+    end crossover;
 
     procedure arrayAtoArrayB(A : in container;B : out container)  is
     begin
@@ -169,7 +131,6 @@ procedure tsp is
 		entry Compute_Fitness(i : integer);
 	end T;
 	
-	--type Task_Arr is array (1..Task_number) of T;
 
 	task body T is
         routine : integer;
@@ -187,7 +148,7 @@ procedure tsp is
 				routine := routine + map(parent(k)(j))(parent(k)(j+1));
 			end loop;
 		    fitness(k) := routine;
-            delay(0.00001);
+            delay(0.001);
         end loop;
         
 	end T;
@@ -211,13 +172,22 @@ procedure tsp is
                     fit := fitness(i+1);
                 end if;
             end loop;
-            if fit<=best then
+            if fit<best then
                 best :=fit;
+                mutation_rate:=100;
                 n:=0;
             else 
                 n:=n+1;
             end if;
-
+            if n>end_condition/4 then 
+                mutation_rate := 75;
+            end if;
+            if n>end_condition/2 then 
+                mutation_rate := 50;
+            end if;
+            if n>3*end_condition/4 then 
+                mutation_rate := 25;
+            end if;
             if n>end_condition then
                 answer:=1;
             else 
@@ -236,6 +206,7 @@ begin
     t_start:=clock;
     reset(g);
     generate_first;
+    generate_map;
     ----------------------------- start find the route
     Find_route:
     loop
@@ -263,16 +234,17 @@ begin
             final_result := fit;
             put(generation,5);
             put(" th generation shortest length : "); 
-            put(final_result,0);
+            put(final_result,3);
             put("    Current shortest length : ");
-            put(best,0);
+            put(best,3);
             new_line;
             generation:=generation+1;
             exit Find_route when result=1; 
             
-            arrayAtoArrayB(parent,childarray);
+            --arrayAtoArrayB(parent,childarray);
             for i in 1..population_size loop
-                childgenerate;
+                --childgenerate;
+                crossover(i);
             end loop;
             arrayAtoArrayB(childarray,parent);
             
@@ -288,4 +260,5 @@ begin
     put("Average execute time for one generation: ");
     Duration_IO.Put(t_exe, 0, 5);
     put_line(" sec");
+    new_line;
 end tsp;
